9b2e00934a39e2a7203ecf33dfd0e9ac9f560c35
Sanjeet Kumar <sanjeet111gupta@gmail.com>
2018-06-20 10:30:10 +0530

    initial

---
+++ Tappy Ball/Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+	public bool gameOver;
+
+	void Awake(){
+		DontDestroyOnLoad (this.gameObject);
+		if (instance == null) {
+			instance = this;
+		} else {
+			Destroy (this.gameObject);
+
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		gameOver = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GameStart(){
+		UiManager.instance.GameStart ();
+		GameObject.Find ("PipeSpawner").GetComponent<PipeSpawner> ().StartSpawningPipes ();
+	}
+	public void GameOver(){
+		gameOver = false;
+		GameObject.Find ("PipeSpawner").GetComponent<PipeSpawner> ().StopSpawningPipes ();
+		ScoreManager.instance.StopScore ();
+		UiManager.instance.GameOver ();
+	}
+}
---
+++ Tappy Ball/Assets/UiManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+	public static UiManager instance;
+	public Text scoreText;
+	public GameObject gameOverPanel;
+	public GameObject startUI;
+	public GameObject GameOvertext;
+	public Text highScoreText;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = ScoreManager.instance.score.ToString ();
+	}
+
+	public void GameOver(){
+		GameOvertext.SetActive (true);
+		highScoreText.text = "HIGH SCORE: " + PlayerPrefs.GetInt ("HighScore");
+		gameOverPanel.SetActive (true);
+	}
+
+	public void GameStart(){
+		startUI.SetActive (false);
+	}
+
+	public void Replay(){
+		SceneManager.LoadScene ("level1");
+	}
+
+	public void Menu(){
+		SceneManager.LoadScene ("Menu");
+	}
+}
